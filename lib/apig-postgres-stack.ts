aa45069aa4e3d88e66f3b7d4599a516f37b979b2
Marc Mendoza <777marc@gmail.com>
2025-06-02 10:18:11 -0400

    add dns rec

--- lib/apig-postgres-stack.ts
+++ lib/apig-postgres-stack.ts
@@ -13,6 +13,8 @@ import {
 } from "aws-cdk-lib/aws-apigateway";
 import { Effect, PolicyStatement, User } from "aws-cdk-lib/aws-iam";
 import { IUserPool } from "aws-cdk-lib/aws-cognito";
+import * as route53 from "aws-cdk-lib/aws-route53";
+import { Certificate } from "aws-cdk-lib/aws-certificatemanager";
 
 interface ApigPostgresStackProps extends cdk.StackProps {
   userPool: IUserPool;
@@ -80,5 +82,40 @@ export class ApigPostgresStack extends cdk.Stack {
       new LambdaIntegration(carsLambda),
       optionsWithAuth
     );
+
+    // Create DNS record
+    const r53Config = {
+      HostedZoneName: "mendoza-code.com",
+      HostedZoneId: "Z012275318RY9D12HI3DG",
+      RecordSet: "cars.mendoza-code.com",
+    };
+
+    // get hosted zone object
+    const zoneObj = route53.HostedZone.fromHostedZoneAttributes(
+      this,
+      "baseZone",
+      {
+        zoneName: r53Config.HostedZoneName,
+        hostedZoneId: r53Config.HostedZoneId,
+      }
+    );
+
+    // create cert
+    const cert: Certificate = new Certificate(this, "mendoza-code", {
+      domainName: r53Config.HostedZoneName,
+    });
+
+    api.addDomainName("cars api", {
+      domainName: r53Config.HostedZoneName,
+      certificate: cert,
+    });
+
+    new route53.ARecord(this, "AliasRecord", {
+      zone: zoneObj,
+      target: route53.RecordTarget.fromAlias(
+        new cdk.aws_route53_targets.ApiGateway(api)
+      ),
+      recordName: r53Config.RecordSet,
+    });
   }
 }
